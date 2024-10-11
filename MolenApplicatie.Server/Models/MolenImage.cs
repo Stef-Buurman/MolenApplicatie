1dfa2aae631056b15a81a35e9e9739875e9303be
Stef <Stef@DESKTOP-V1O78L5>
2024-10-11 19:13:26 +0200

    Removed image from db and removed molens which we dont need

--- MolenApplicatie.Server/Models/MolenImage.cs
+++ MolenApplicatie.Server/Models/MolenImage.cs
@@ -1,13 +1,8 @@
-﻿using SQLite;
-
-namespace MolenApplicatie.Server.Models
+﻿namespace MolenApplicatie.Server.Models
 {
     public class MolenImage
     {
-        [PrimaryKey, AutoIncrement]
-        public int MolenImageId { get; set; }
-        public int MolenDataId { get; set; }
-        public string MolenTBN { get; set; }
+        public string FileName { get; set; }
         public byte[] Image { get; set; }
     }
 }
