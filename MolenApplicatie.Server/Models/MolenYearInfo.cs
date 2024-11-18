972eed6d5a16164c631f0171e720791f22bfe292
Stef <st3fbuurman@gmail.com>
2024-11-18 18:55:19 +0100

    New Molendata

---
+++ MolenApplicatie.Server/Models/MolenYearInfo.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace MolenApplicatie.Server.Models
+{
+    public class MolenYearInfo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Status_before { get; set; }
+        public int Year { get; set; }
+        public string Status_after { get; set; }
+        public int MolenDataId { get; set; }
+    }
+}
