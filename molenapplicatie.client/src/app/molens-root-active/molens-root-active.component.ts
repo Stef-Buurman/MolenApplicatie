97e7670e6548a9b31c53bab76316f0bcfd5103ea
Stef <st3fbuurman@gmail.com>
2024-11-25 23:10:27 +0100

    created root for molens

---
+++ molenapplicatie.client/src/app/molens-root-active/molens-root-active.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-molens-root-active',
+  templateUrl: './molens-root-active.component.html',
+  styleUrl: './molens-root-active.component.scss'
+})
+export class MolensRootActiveComponent {
+
+}
