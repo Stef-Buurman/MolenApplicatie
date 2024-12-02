9450a394b56d99ade85283c8e6ccee35072d6164
Stef <st3fbuurman@gmail.com>
2024-12-02 23:17:43 +0100

    Enhance upload dialog with new UI and functionality
    Updated the HTML template to provide a comprehensive user interface for uploading images, including sections for file selection, file details, API key input, upload button, status messages, and image preview.
    
    Styled the new HTML elements in the SCSS file to ensure a visually appealing and user-friendly interface, with specific styles for the container, image details, image preview, file input container, file input, and file label.
    
    Extended the TypeScript component to handle the logic for file selection, image preview, API key input, and uploading the image to a server. Added error handling and success notifications to improve the user experience.

--- molenapplicatie.client/src/app/dialogs/upload-image-dialog/upload-image-dialog.component.ts
+++ molenapplicatie.client/src/app/dialogs/upload-image-dialog/upload-image-dialog.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MolenDataClass } from '../../../Class/MolenDataClass';
+import { MolenImage } from '../../../Class/MolenImage';
+import { MolenData } from '../../../Interfaces/MolenData';
+import { MolenService } from '../../../Services/MolenService';
+import { Toasts } from '../../../Utils/Toasts';
 
 @Component({
   selector: 'app-upload-image-dialog',
@@ -6,5 +12,91 @@ import { Component } from '@angular/core';
   styleUrl: './upload-image-dialog.component.scss'
 })
 export class UploadImageDialogComponent {
+  public molen?: MolenData;
+  public status: "initial" | "uploading" | "success" | "fail" = "initial";
+  public file: File | null = null;
+  public imagePreview: string | null = null;
+  public APIKey: string = "";
+  public molenImages: MolenImage[] = [];
+  public selectedImage?: MolenImage;
 
+  public imagesAdded: boolean = false;
+  get HasImagesLeft(): boolean {
+    if (this.molen == undefined || this.molen.addedImages == undefined) return false;
+    return this.molen.addedImages.length > 0;
+  }
+
+  isExpanded = false;
+
+  constructor(private toasts: Toasts,
+    private cdr: ChangeDetectorRef,
+    private molenService: MolenService,
+    private dialogRef: MatDialogRef<UploadImageDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { molen: MolenData }
+  ) { }
+
+  ngOnInit(): void {
+    this.molen = this.data.molen;
+  }
+
+  ngOnDestroy(): void {
+    this.onClose();
+  }
+
+  onClose(): void {
+    this.dialogRef.close(this.molen);
+  }
+
+  expandDetails() {
+    this.isExpanded = !this.isExpanded
+  }
+
+  removeImg(): void {
+    this.file = null;
+  }
+
+  onFileSelected(event: any): void {
+    const uploadedFile: File = event.target.files[0];
+
+    if (uploadedFile) {
+      this.status = "initial";
+      this.file = uploadedFile;
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.imagePreview = e.target?.result as string;
+      };
+      reader.readAsDataURL(this.file);
+    }
+  }
+
+  onSubmit(): void {
+    if (this.file && this.molen) {
+      this.status = "uploading";
+
+      const formData = new FormData();
+      formData.append('image', this.file, this.file.name);
+
+      var previousMolenImages: MolenImage[] = this.molen.addedImages ?? [];
+
+      this.molenService.uploadImage(this.molen.ten_Brugge_Nr, formData, this.APIKey).subscribe({
+        next: (molen: MolenData) => {
+          this.molen = molen;
+        },
+        error: (error) => {
+          this.status = "fail";
+          if (error.status == 401) {
+            this.toasts.showError("Er is een verkeerde api key ingevuld!");
+          } else {
+            this.toasts.showError(error.error.message);
+          }
+        },
+        complete: () => {
+          this.removeImg();
+          this.toasts.showSuccess("Image is saved successfully!");
+
+        }
+      });
+      this.APIKey = "";
+    }
+  }
 }
