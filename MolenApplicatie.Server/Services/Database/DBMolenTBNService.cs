3f79d1885a61697e62f6508f50c01cce98190bbb
Stef <st3fbuurman@gmail.com>
2025-07-02 22:12:04 +0200

    Refactor DBMolenTBNService to remove unused context field and commented-out Add method

--- MolenApplicatie.Server/Services/Database/DBMolenTBNService.cs
+++ MolenApplicatie.Server/Services/Database/DBMolenTBNService.cs
@@ -7,23 +7,11 @@ namespace MolenApplicatie.Server.Services.Database
 {
     public class DBMolenTBNService : DBDefaultService<MolenTBN>
     { 
-        private readonly MolenDbContext _context;
         public DBMolenTBNService(MolenDbContext context) : base(context)
-        {
-            _context = context;
-        }
+        {}
         public override bool Exists(MolenTBN molenTBN, out MolenTBN? existing)
         {
             return Exists(e => e.Ten_Brugge_Nr == molenTBN.Ten_Brugge_Nr, out existing);
-        }
-        public override async Task<MolenTBN> Add(MolenTBN molenType)
-        {
-            if (molenType == null) return null;
-            if (Exists(molenType, out MolenTBN? existingEntity))
-            {
-                return existingEntity;
-            }
-            return await base.Add(molenType);
         }
     }
 }
