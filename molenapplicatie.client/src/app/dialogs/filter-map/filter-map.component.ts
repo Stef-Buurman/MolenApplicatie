07f47a0ee9d54a69ecfcf9507e15542ead8a1e02
Stef <st3fbuurman@gmail.com>
2025-07-19 22:50:11 +0200

    Refactor filter map component to use dropdowns for filters and update state management

--- molenapplicatie.client/src/app/dialogs/filter-map/filter-map.component.ts
+++ molenapplicatie.client/src/app/dialogs/filter-map/filter-map.component.ts
@@ -1,56 +1,115 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FilterFormValues } from '../../../Interfaces/Filters/Filter';
 import { MolenService } from '../../../Services/MolenService';
 import { Toasts } from '../../../Utils/Toasts';
+import { MolenFilterList } from '../../../Interfaces/Filters/MolenFilterList';
+import { MolenFilters } from '../../../Interfaces/Filters/MolenFilters';
 
 @Component({
   selector: 'app-filter-map',
   templateUrl: './filter-map.component.html',
-  styleUrl: './filter-map.component.scss'
+  styleUrl: './filter-map.component.scss',
 })
 export class FilterMapComponent implements OnInit {
-  selectedOption: string = "";
-  provincies: string[] = [];
-  provincie: string = "";
-  constructor(private dialogRef: MatDialogRef<FilterMapComponent>,
+  selectedFilter: MolenFilters = {
+    provincie: '',
+    toestand: '',
+    type: '',
+  };
+  provincie: string = '';
+  molenFilters: MolenFilterList = { provincies: [], toestanden: [], types: [] };
+  filters: { [name: string]: FilterFormValues } = {};
+  constructor(
+    private dialogRef: MatDialogRef<FilterMapComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { filters: FilterFormValues[] },
     private toasts: Toasts,
-    private router: Router,
-    private molenService:MolenService,
-    private route: ActivatedRoute) { }
+    private molenService: MolenService
+  ) {}
 
   ngOnInit() {
-    this.molenService.getAllMolenProvincies().subscribe({
-      next: (provincies:string[]) => {
-        this.provincies = provincies;
-        if (this.router.url) {
-          var urlStructure = this.router.url.split("/").filter(str => str !== "");
-          if (urlStructure.length == 1) {
-            this.selectedOption = urlStructure[0];
-          } else if (urlStructure.length >= 2) {
-            this.selectedOption = urlStructure[0];
-            this.provincie = urlStructure[1].toLowerCase();
-          }
-        }
-      }
-    })
+    let filters = this.data.filters || [];
+    filters.forEach((filter) => {
+      this.filters[filter.filterName] = filter;
+    });
+    if (this.filters['Provincie']) {
+      const provincieValue = this.filters['Provincie'].value;
+      this.selectedFilter.provincie =
+        typeof provincieValue === 'string' ? provincieValue : undefined;
+    }
+    if (this.filters['MolenState']) {
+      const toestandValue = this.filters['MolenState'].value;
+      this.selectedFilter.toestand =
+        typeof toestandValue === 'string' ? toestandValue : undefined;
+    }
+    if (this.filters['MolenType']) {
+      const molenTypeValue = this.filters['MolenType'].value;
+      this.selectedFilter.type =
+        typeof molenTypeValue === 'string' ? molenTypeValue : undefined;
+    }
+    this.molenService.getAllMolenFilters().subscribe({
+      next: (filters: MolenFilterList) => {
+        this.molenFilters = filters;
+      },
+    });
   }
 
-  onClose() {
-    this.dialogRef.close();
+  onClose(filters: FilterFormValues[] | undefined = undefined) {
+    this.dialogRef.close(filters);
   }
 
   filterMap() {
-    var targetUrl = "/" + this.selectedOption;
-    if (this.selectedOption == "disappeared") {
-      if (this.provincie) {
-        targetUrl += "/" + this.provincie
+    if (this.selectedFilter.toestand) {
+      if (!this.filters['MolenState']) {
+        this.filters['MolenState'] = {
+          filterName: 'MolenState',
+          value: this.selectedFilter.toestand || '',
+          type: 'string',
+          isAList: false,
+          name: 'Molen state',
+        };
       } else {
-        this.toasts.showError("Je hebt geen provincie gekozen!")
-        return
+        this.filters['MolenState'].value = this.selectedFilter.toestand || '';
       }
     }
-    this.router.navigateByUrl(targetUrl);
-    this.onClose();
+
+    if (this.selectedFilter.provincie) {
+      if (!this.filters['Provincie']) {
+        this.filters['Provincie'] = {
+          filterName: 'Provincie',
+          value: this.selectedFilter.provincie || '',
+          type: 'string',
+          isAList: false,
+          name: 'Provincie',
+        };
+      } else {
+        this.filters['Provincie'].value = this.selectedFilter.provincie || '';
+      }
+    }
+
+    if (
+      this.filters['MolenState'] &&
+      !this.filters['Provincie'] &&
+      (this.selectedFilter.toestand === 'disappeared' ||
+        this.filters['MolenState'].value === 'disappeared')
+    ) {
+      this.toasts.showError('Je hebt geen provincie gekozen!');
+      return;
+    }
+
+    if (this.selectedFilter.type) {
+      if (!this.filters['MolenType']) {
+        this.filters['MolenType'] = {
+          filterName: 'MolenType',
+          value: this.selectedFilter.type || '',
+          type: 'string',
+          isAList: false,
+          name: 'Molen type',
+        };
+      } else {
+        this.filters['MolenType'].value = this.selectedFilter.type || '';
+      }
+    }
+    this.onClose(Object.values(this.filters));
   }
 }
